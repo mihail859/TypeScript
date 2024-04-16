47acbced80668d716ee63a952106cb392a6f4b4b
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-04-16 21:02:40 +0300

    Create inventory.ts

---
+++ Advanced Data Types and Interfaces/src/tasks/inventory.ts
@@ -0,0 +1,20 @@
+// input:
+const data: string[] =  [
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+]
+
+// interfaces
+interface heroObjInterface{
+    [key: string]: HeroData
+}
+
+interface HeroData{
+    level: number;
+    items: string[] | null
+}
+
+const heroObj: heroObjInterface = {}
+
+// Logic
