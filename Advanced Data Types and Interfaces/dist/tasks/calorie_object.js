59bd28b7560056d03c723ef91ddca4c03d258f59
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-04-16 20:37:11 +0300

    update

--- Advanced Data Types and Interfaces/dist/tasks/calorie_object.js
+++ Advanced Data Types and Interfaces/dist/tasks/calorie_object.js
@@ -1,2 +1,8 @@
 "use strict";
+// Input
 const input = ['Yoghurt', '48', 'Rise', '138', 'Apple', '52'];
+// Solving logic
+for (let index = 0; index < input.length; index++) {
+    const element = input[index];
+    console.log(element);
+}
--- Advanced Data Types and Interfaces/src/tasks/calorie_object.ts
+++ Advanced Data Types and Interfaces/src/tasks/calorie_object.ts
@@ -1,1 +1,14 @@
+// Input
 const input: string[] = ['Yoghurt', '48', 'Rise', '138', 'Apple', '52'];
+
+//Interfaces:
+interface ProductInterface {
+    name: string;
+    quantity: number;
+}
+
+// Solving logic
+for (let index = 0; index < input.length; index++) {
+    const element = input[index];
+    console.log(element)
+}
