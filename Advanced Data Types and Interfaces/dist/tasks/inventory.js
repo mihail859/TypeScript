a0dcfc181fdf453cd8e0aa752fa02df68e708b68
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-04-16 21:35:11 +0300

    solve

--- Advanced Data Types and Interfaces/dist/tasks/inventory.js
+++ Advanced Data Types and Interfaces/dist/tasks/inventory.js
@@ -3,10 +3,31 @@
 const data = [
     'Isacc / 25 / Apple, GravityGun',
     'Derek / 12 / BarrelVest, DestructionSword',
-    'Hes / 1 / Desolator, Sentinel, Antara'
+    'Hes / 1 / Desolator, Sentinel, Antara',
+    'Hes / 5 / Desolator, Sentinel, Antara, Shadow'
 ];
 const heroObj = {};
 // Logic
 for (let row of data) {
-    console.log(row);
+    const splitInformation = row.split(" / ");
+    const name = splitInformation[0];
+    const level = Number(splitInformation[1]);
+    const items = splitInformation[2].split(", ");
+    if (heroObj.hasOwnProperty(name)) {
+        if (level !== undefined) {
+            heroObj[name].level = level;
+        }
+        items.forEach(item => {
+            heroObj[name].items.push(item);
+        });
+    }
+    else {
+        heroObj[name] = {
+            level: level,
+            "items": items
+        };
+    }
 }
+const newObjEntries = Object.entries(heroObj).sort((a, b) => a[0].localeCompare(b[0]));
+const sortedObj = Object.fromEntries(newObjEntries);
+console.log(sortedObj);
--- Advanced Data Types and Interfaces/src/tasks/inventory.ts
+++ Advanced Data Types and Interfaces/src/tasks/inventory.ts
@@ -2,7 +2,8 @@
 const data: string[] =  [
     'Isacc / 25 / Apple, GravityGun',
     'Derek / 12 / BarrelVest, DestructionSword',
-    'Hes / 1 / Desolator, Sentinel, Antara'
+    'Hes / 1 / Desolator, Sentinel, Antara',
+    'Hes / 5 / Desolator, Sentinel, Antara, Shadow'
 ]
 
 // interfaces
@@ -12,12 +13,33 @@ interface heroObjInterface{
 
 interface HeroData{
     level: number;
-    items: string[] | null
+    items: string[]
 }
 
 const heroObj: heroObjInterface = {}
 
 // Logic
 for (let row of data){
-    console.log(row)
+    const splitInformation: string[] = row.split( " / " );
+    const name: string = splitInformation[0];
+    const level: number = Number(splitInformation[1]);
+    const items: string[] = splitInformation[2].split(", ")
+
+    if (heroObj.hasOwnProperty(name)){
+        if (level !== undefined){
+            heroObj[name].level = level;
+        }
+       items.forEach(item =>{
+        heroObj[name].items.push(item);
+       })
+    }else{
+        heroObj[name] = {
+            level: level,
+            "items": items
+        }
+    }
 }
+
+const newObjEntries = Object.entries(heroObj).sort(( a, b ) => a[0].localeCompare(b[0]))
+const sortedObj: heroObjInterface = Object.fromEntries(newObjEntries);
+console.log(sortedObj)
