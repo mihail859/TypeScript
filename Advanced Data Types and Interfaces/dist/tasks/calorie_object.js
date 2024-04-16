58116e77dc89990eb12ef7b4f545df0d1f244b14
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-04-16 20:45:57 +0300

    solve

--- Advanced Data Types and Interfaces/dist/tasks/calorie_object.js
+++ Advanced Data Types and Interfaces/dist/tasks/calorie_object.js
@@ -1,8 +1,17 @@
 "use strict";
 // Input
-const input = ['Yoghurt', '48', 'Rise', '138', 'Apple', '52'];
+const input = ['Yoghurt', '48', 'Rise', '138', 'Apple', '52', 'Yoghurt', '10000'];
+//Output 
+const outputObj = {};
 // Solving logic
-for (let index = 0; index < input.length; index++) {
-    const element = input[index];
-    console.log(element);
+for (let index = 0; index < input.length; index += 2) {
+    const product = input[index];
+    const quantity = Number(input[index + 1]);
+    if (outputObj.hasOwnProperty(product)) {
+        outputObj[product] += quantity;
+    }
+    else {
+        outputObj[product] = quantity;
+    }
 }
+console.log(outputObj);
--- Advanced Data Types and Interfaces/src/tasks/calorie_object.ts
+++ Advanced Data Types and Interfaces/src/tasks/calorie_object.ts
@@ -1,14 +1,22 @@
 // Input
-const input: string[] = ['Yoghurt', '48', 'Rise', '138', 'Apple', '52'];
+const input: string[] = ['Yoghurt', '48', 'Rise', '138', 'Apple', '52', 'Yoghurt', '10000'];
 
 //Interfaces:
 interface ProductInterface {
-    name: string;
-    quantity: number;
+    [key: string]: number;
 }
+//Output 
+const outputObj: ProductInterface = {}
 
 // Solving logic
-for (let index = 0; index < input.length; index++) {
-    const element = input[index];
-    console.log(element)
+for (let index = 0; index < input.length; index+=2) {
+    const product: string = input[index];
+    const quantity: number = Number(input[index+1]);
+    if (outputObj.hasOwnProperty(product)) {
+        outputObj[product] += quantity;
+    } else{
+        outputObj[product] = quantity;
+    }
 }
+
+console.log(outputObj)
