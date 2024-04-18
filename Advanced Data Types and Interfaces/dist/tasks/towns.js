d8944222715e123b979cd714b80379713a4fae22
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-04-18 09:35:03 +0300

    solved

--- Advanced Data Types and Interfaces/dist/tasks/towns.js
+++ Advanced Data Types and Interfaces/dist/tasks/towns.js
@@ -2,3 +2,23 @@
 // Input
 let inputData = ['Sofia | 42.696552 | 23.32601',
     'Beijing | 39.913818 | 116.363625'];
+//? class Town
+class Town {
+    town = "";
+    latitude = "";
+    longitude = "";
+    constructor(town, latitude, longitude) {
+        this.town = town;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
+// Creating town Objects
+for (let town of inputData) {
+    let dataSplit = town.split(" | ");
+    let name = dataSplit[0];
+    let latitude = dataSplit[1];
+    let longitude = dataSplit[2];
+    const newObj = new Town(name, latitude, longitude);
+    console.log(newObj);
+}
--- Advanced Data Types and Interfaces/src/tasks/towns.ts
+++ Advanced Data Types and Interfaces/src/tasks/towns.ts
@@ -3,3 +3,36 @@ let  inputData: string[] = ['Sofia | 42.696552 | 23.32601',
 'Beijing | 39.913818 | 116.363625'];
 
 
+//Output
+// { town: 'Sofia', latitude: '42.70', longitude: '23.33' }
+// { town: 'Beijing', latitude: '39.91', longitude: '116.36' }
+
+//!interface Town
+interface TownInterface{
+    town: string;
+    latitude: string;
+    longitude: string;
+}
+
+//? class Town
+class Town implements TownInterface{
+    town = "";
+    latitude= "";
+    longitude = "";
+    constructor (town: string, latitude: string, longitude: string){
+        this.town = town;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
+
+// Creating town Objects
+for (let town of inputData){
+    let dataSplit = town.split(" | ")
+    let name: string = dataSplit[0];
+    let latitude: string = dataSplit[1];
+    let longitude: string = dataSplit[2];
+
+    const newObj: Town = new Town(name, latitude, longitude)
+    console.log(newObj)
+}
