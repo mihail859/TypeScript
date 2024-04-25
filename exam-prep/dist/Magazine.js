1468b7a7df29d25e3e08df008a6403be23f52b1e
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-04-25 22:01:11 +0300

    solve

--- exam-prep/dist/Magazine.js
+++ exam-prep/dist/Magazine.js
@@ -1,67 +1,54 @@
-import { Cloth } from "./Cloth";
-
-export class Magazine{
-
-    private type: string;
-    private capacity: number;
-    private clothes: Cloth[]
-
-    constructor(type: string, capacity: number) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Magazine = void 0;
+class Magazine {
+    type;
+    capacity;
+    clothes;
+    constructor(type, capacity) {
         this.type = type;
         this.capacity = capacity;
         this.clothes = [];
     }
-
-    addCloth(cloth: Cloth): void{
-        if (this.clothes.length < this.capacity){
+    addCloth(cloth) {
+        if (this.clothes.length < this.capacity) {
             this.clothes.push(cloth);
         }
     }
-
-    removeCloth( color: string ): boolean {
+    removeCloth(color) {
         let index = this.clothes.findIndex(c => c.color === color);
-
-        if (index !== -1){
+        if (index !== -1) {
             this.clothes.splice(index, 1);
             return true;
         }
         return false;
     }
-
-    getSortedClothes(): Cloth[] {
+    getSortedClothes() {
         let sorted = this.clothes.sort((a, b) => a.size - b.size);
-
         return sorted;
     }
-    
-    getSmallestCloth(): Cloth{
-        if (!this.clothes.length){
-            return { } as Cloth;
+    getSmallestCloth() {
+        if (!this.clothes.length) {
+            return {};
         }
-
-        if (this.clothes.length === 1){
+        if (this.clothes.length === 1) {
             return this.clothes[0];
         }
-
         const smallestCloth = this.getSortedClothes()[0];
         return smallestCloth;
     }
-
-    getCloth(color: string): Cloth{
+    getCloth(color) {
         const searchedCloth = this.clothes.find(cl => cl.color === color);
-        return searchedCloth!;
+        return searchedCloth;
     }
-
-    getClothCount(): number{
+    getClothCount() {
         return this.clothes.length;
     }
-
-    report(): string{
+    report() {
         const sortClothes = this.getSortedClothes();
         const clothesForReport = sortClothes.map((c) => `Product: ${c.type} with size ${c.size}, color ${c.color}`).join("\n");
-
-    const report = `${this.type} magazine contains:\n${clothesForReport}`;
-    return report;
+        const report = `${this.type} magazine contains:\n${clothesForReport}`;
+        return report;
     }
-
 }
+exports.Magazine = Magazine;
